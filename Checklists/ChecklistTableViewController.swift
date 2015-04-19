a58cdfcad72c119e0e30ff26d44f9072ad75ca69
Raghav Mangrola <raghav.mangrola@gmail.com>
2015-04-19 10:55:50 -0700

    Initial project set up
    Removed ViewController and default View with
    ChecklistTableViewController and a Table View

---
+++ Checklists/ChecklistTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChecklistTableViewController.swift
+//  Checklists
+//
+//  Created by Raghav Mangrola on 4/19/15.
+//  Copyright (c) 2015 Raghav Mangrola. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
