5e87cdcb6891b840de19cf43f8dc9532d6e62d4c
Raghav Mangrola <raghav.mangrola@gmail.com>
2015-04-26 21:14:17 -0700

    Created View Controller for Add Item Scene
    Set up Cancel and Done buttons to close the view controller

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Raghav Mangrola on 4/26/15.
+//  Copyright (c) 2015 Raghav Mangrola. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    @IBAction func cancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func done() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
